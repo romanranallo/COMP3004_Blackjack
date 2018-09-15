328e6f769c25293051d728be4286065db9e5480d
Roman Ranallo <romanranallo@cmail.carleton.ca>
2018-09-15 14:07:02 -0400

    Added Junits for Card object constructor

---
+++ src_test/cards/CardTest.java
@@ -0,0 +1,28 @@
+package cards;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class CardTest {
+	
+	@Test
+	public void testCtr() {
+		
+		Card c = new Card("H2");
+		assertTrue(c.getSuit().equals("H") && c.getValue() == 2);
+		
+		c = new Card("SK");
+		assertTrue(c.getSuit.equals("S") && c.getValue() == 10);
+		
+		c = new Card("CQ");
+		assertTrue(c.getSuit.equals("C") && c.getValue() == 10);
+		
+		c = new Card("HJ");
+		assertTrue(c.getSuit.equals("H") && c.getValue() == 10);
+		
+		c = new Card("DA");
+		// Ace should default to 11
+		assertTrue(c.getSuit().equals("D") && c.getValue() == 11);
+	}
+
+}
