627b67e7ce1bbd785584a438b3c25658e67e219e
Roman Ranallo <romanranallo@cmail.carleton.ca>
2018-09-18 10:15:01 -0400

    Add player object. No JUnits since all methods are trivial.

---
+++ src/player/Player.java
@@ -0,0 +1,25 @@
+package player;
+
+import java.util.ArrayList;
+
+import cards.Card;
+
+public class Player {
+	
+	private ArrayList<Card> hand;
+	private int score;
+	
+	public Player() { 
+		this.hand = new ArrayList<Card>();
+		this.score = 0;
+	}
+	
+	public ArrayList<Card> getHand() { return this.hand; }
+	public int getScore() { return this.score; }
+	public void setScore(int score) { this.score = score; }
+	
+	public void addCard(Card c) {
+		this.hand.add(c);
+	}
+	
+}
