9a4545a81bc0c05efbff37be0d3f2b05afbcb8d7
Roman Ranallo <romanranallo@cmail.carleton.ca>
2018-09-17 22:05:50 -0400

    Updates to Card test for visibility in console output.

--- src_test/cards/CardTest.java
+++ src_test/cards/CardTest.java
@@ -26,6 +26,40 @@ public class CardTest {
 		c = new Card("DA");
 		// Ace should default to 11
 		assertTrue(c.getSuit().equals("D") && c.getValue() == 11);
+
+		c = new Card("SK");
+		assertTrue(c.getSuit().equals("S") && c.getValue() == 10);
+		assertTrue(c instanceof King);
+		
+		c = new Card("CQ");
+		assertTrue(c.getSuit().equals("C") && c.getValue() == 10);
+		assertTrue(c instanceof Queen);
+		
+		c = new Card("HJ");
+		assertTrue(c.getSuit().equals("H") && c.getValue() == 10);
+		assertTrue(c instanceof Jack);
+		
+		c = new Card("DA");
+		// Ace should default to 11
+		assertTrue(c.getSuit().equals("D") && c.getValue() == 11);
+		assertTrue(c instanceof Ace);
+	}
+	
+	@Test
+	public void testVisibility() {
+		Card c = new Card("SA");
+		assertTrue(c.isVisible());
+		
+		c = new Card("DA", true);
+		assertTrue(c.isVisible());
+		c.setVisible(false);
+		assertFalse(c.isVisible());
+		
+		c = new Card("HJ", false);
+		assertFalse(c.isVisible());
+		c.setVisible(true);
+		assertTrue(c.isVisible());
+		 
 	}
 
 }
